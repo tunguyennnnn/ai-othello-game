f4a2597463a7423fb29583d64f13c39d8ddd4a41
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-11-27 14:43:43 -0500

    Default testing to minimax 3

--- testJavier.sh
+++ testJavier.sh
@@ -2,10 +2,10 @@
 # A quick iteration test against myself
 TEST_A="rando"
 TEST_B="greedy win"
-TEST_C="minimax 4"
+TEST_C="minimax 3"
 #TEST_D="montecarlo" #Not done yet :/
 
-BLACK_TEST=$TEST_A
+BLACK_TEST=$TEST_C
 WHITE_TEST=$TEST_A
 cd framework
 python main.py "python -u ../javier/main.py W $WHITE_TEST" "python -u ../javier/main.py B $BLACK_TEST"
